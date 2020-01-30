c671ce12905d92f03ff941ea6285106c19ecc027
John <jcmeier22@gmail.com>
2020-01-30 12:48:29 -0500

    resolved merge conflict for real

--- src/app/report.service.ts
+++ src/app/report.service.ts
@@ -10,11 +10,7 @@ export class ReportService {
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type':  'application/json',
-<<<<<<< HEAD
       'Authorization': `Bearer  ${localStorage.getItem('token')}`
-=======
-      'Authorization': 'Bearer  OVf8kvEUPF-wTOmfIDRe0Tq8at4vGzNyee5XKVvAarvDDGWEDMQ-nBfGKa_BPq-tx0GLlRTsDH7l-m8JiIO4hTW4IWR9RmeQmRTzbKmN4ysJLcU1XZS8JqE0n4lIv82XVlBLZgRHI5INBAVXFNrBAP4pSEd5F4uZcx3MjHARwGwNN7Eu775dLOzPOJZlQm4CqsZ9-6n5jpTW0w5q1NRUtekEAfQ_oODvfwEsaITafFgrOu5XVr2wcn5sROPARR4FR0erH9YaOkwIZN60LMDGAil2_zgQ52dVcmnJESXFulzjeVRD30dlAD1qItEZbmBd'
->>>>>>> 2251b426e7d69607b10ffff854376e326e2e2a7e
     })
   };
 
